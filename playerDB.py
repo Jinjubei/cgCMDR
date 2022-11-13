88fef89eb235affb67cefd71905ae4c18609fe96
Jinjubei <Jinjubei@gmail.com>
2022-11-13 14:44:35 -0500

    Create playerDB.py
    Sample database.

---
+++ playerDB.py
@@ -0,0 +1,17 @@
+#Sample player DB(JSON file)
+{
+  players = [
+    {name:"Jhin Wiles",
+     MMR:10
+    },
+    {name:"Pat Healey",
+     MMR:8.75
+    },
+    {name:"Brian Eastman",
+     MMR:12
+    },
+    {name:"Chris Fraizer",
+     MMR:3
+    }
+  ]
+}
