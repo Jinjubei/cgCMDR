27608cb3605a231ac011636020654f934b16d37e
Jinjubei <Jinjubei@gmail.com>
2022-11-13 20:22:56 +0000

    added functionality for importing player DB

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-import player
+#! /bin/python3
+from player import Player
+import json
 
 def main():
+  global players
   players = []
+  importDB()
+  
+def importDB():
+  f = open("playerDB.json")
+  db = json.load(f)
 
+  for player in db['players']:
+    players.append(Player(player['name'],player['MMR']))
 
+  print(players)
 
 
 #LIST OF FUNCTIONS TO WRITE
-#TODO:Prompt for players DB
-#TODO:Prompt for players list
-#TODO:Build pods
-#TODO:Utility to start new season
-#TODO:cEDH toggle?
+#Prompt for players list
+#Build pods
+#Utility to start new season
+#cEDH toggle?
 
 
 
