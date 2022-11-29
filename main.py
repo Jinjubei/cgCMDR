1eabd6e652fc5e8588962e7520ee18e5fc8c198e
Jhin Quinlan <jinjubei@gmail.com>
2022-11-28 21:05:33 -0500

    set the ground work for building pods

--- main.py
+++ main.py
@@ -16,6 +16,31 @@ def importDB():
 
   print(players)
 
+#figure out how many 3 and 4 man pods are needed.
+def numPods(numple):
+  numple = int(numple)
+  
+  if (numple == 3):
+    return [1,0]
+  elif (numple == 4):
+    return [0,1]
+  elif(numple%4 == 0):
+    return [0,numple/4]
+  else:
+    threes = 4-numple%4
+    return [threes,numple-threes/4]
+
+def genPods():
+  if(len(players) == 1 or len(players) ==2):
+    #TODO:Admonish TO for trying to make a pod with 1 or 2 players
+  if(len(players) == 3 or len(players) == 4 or len(players) == 5):
+    #TODO: Make one pod of size 3,4,5
+  
+  Pods = numPods(len(players))
+  #TODO: make 3 and 4 person pods
+  Pods[0] #3 person pods
+  Pods[1] #4 person pods
+
 
 #LIST OF FUNCTIONS TO WRITE
 #Prompt for players list
