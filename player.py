0bc907497c5aafb49ad31eb7839ce47723619a34
regularwave <regularwave@gmail.com>
2022-12-01 14:37:57 -0500

    clean directory

--- player.py
+++
@@ -1,7 +0,0 @@
-class Player:
-  def __init__(self,name,MMR):
-    self.name = name
-    self.MMR = MMR
-#TODO: algorithim for determining MMR
-  def set_mmr(self,placement):
-    self.MMR = self.MMR+placement
